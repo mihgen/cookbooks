b7fb25d50a1a6ed880d6ebd13410b4de9e58795b
Mike Scherbakov <mihgen@gmail.com>
2010-07-04 14:30:57 +0400

    Added template activisation to Postgresql cookbook

--- postgresql/recipes/server.rb
+++ postgresql/recipes/server.rb
@@ -20,11 +20,6 @@
 include_recipe "postgresql::client" 
 
 package "postgresql"
-package "oidentd"
-
-service "oidentd" do
-  action [ :enable, :start ]
-end
 
 case node[:platform]
 when "centos","fedora"
@@ -52,13 +47,13 @@ when "centos","fedora"
   end
 end
 
-#template "#{node[:postgresql][:dir]}/pg_hba.conf" do
-  #source "pg_hba.conf.erb"
-  #owner "postgres"
-  #group "postgres"
-  #mode 0600
-  #notifies :reload, resources(:service => "postgresql")
-#end
+template "#{node[:postgresql][:dir]}/pg_hba.conf" do
+  source "pg_hba.conf.erb"
+  owner "postgres"
+  group "postgres"
+  mode 0600
+  notifies :reload, resources(:service => "postgresql")
+end
 
 #template "#{node[:postgresql][:dir]}/postgresql.conf" do
   #source "postgresql.conf.erb"
