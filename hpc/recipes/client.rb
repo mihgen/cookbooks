3f3a0c117e8fb2ab802047298dc12855edbd57d8
Mike Scherbakov <mihgen@gmail.com>
2010-09-26 01:34:21 +0400

    Added mount in hpc::client

--- hpc/recipes/client.rb
+++ hpc/recipes/client.rb
@@ -6,3 +6,13 @@ opkg-sc3
 opkg-switcher
 opkg-torque
 }.each { |pkg| package pkg }
+
+server = search(:node, "*:*").select { |e| e.run_list.run_list_items.select{ |i| i.name == "hpc::server" }.any? }.map{ |e| e["ipaddress"] }
+
+mount "/home" do
+  device "#{server}:/home"
+  fstype "nfs"
+  options "rw"
+  action [:mount, :enable]
+end
+
