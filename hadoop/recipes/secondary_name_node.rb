c46ab15ea1a42e096f289bbd735d2a28069bf074
Mike Scherbakov <mihgen@gmail.com>
2010-11-16 14:40:00 +0300

    [Hadoop] Removed unneeded secondarynodes template

--- hadoop/recipes/secondary_name_node.rb
+++ hadoop/recipes/secondary_name_node.rb
@@ -1,11 +1,1 @@
 include_recipe "hadoop"
-
-template "#{node[:hadoop][:conf_dir]}/secondarynamenodes" do
-  owner node[:hadoop][:user]
-  group node[:hadoop][:user]
-  mode 0644
-  source "secondarynamenodes.erb"
-  variables({
-    :hosts => search(:node, %q{run_list:"recipe[hadoop::secondary_name_node]"}).map{ |e| e["fqdn"] }
-  })
-end
