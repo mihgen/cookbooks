d022b6efc324787aaf1e55d503a6075f202a6972
Mike Scherbakov <mihgen@gmail.com>
2011-01-14 10:47:24 +0300

    [simple-httpd] Added metadata

---
+++ simple-httpd/metadata.rb
@@ -0,0 +1,7 @@
+maintainer        "Mike Scherbakov"
+maintainer_email  "mihgen@gmail.com"
+license           "Apache 2.0"
+description       "Installs and creates demo index.html"
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version           "0.1"
+supports          "Fedora", "CentOS"
--- simple-httpd/recipes/demo.rb
+++ simple-httpd/recipes/demo.rb
@@ -1,3 +1,5 @@
+include_recipe "simple-httpd"
+
 template "/var/www/html/index.html" do
   mode 0644
   source "index.html.erb"
